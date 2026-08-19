8c2e2a204704fd7af99b9cfebfff6cb0edcfbf95
Rafli <rafli4324@users.noreply.github.com>
2026-08-20 01:50:33 +0700

    Fix syntax for updateOrCreate in UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,28 +2,29 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        \App\Models\User::updateOrCreate([
-            'name' => 'Administrator',
-            'username' => 'admin',
-            'password' => bcrypt('admin'),
-            'role' => 'admin',
-        ]);
+        User::updateOrCreate(
+            ['username' => 'admin'],
+            [
+                'name' => 'Administrator',
+                'password' => bcrypt('admin'),
+                'role' => 'admin',
+            ]
+        );
 
-        \App\Models\User::updateOrCreate([
-            'name' => 'Kasir',
-            'username' => 'kasir',
-            'password' => bcrypt('kasir'),
-            'role' => 'kasir',
-        ]);
+        User::updateOrCreate(
+            ['username' => 'kasir'],
+            [
+                'name' => 'Kasir',
+                'password' => bcrypt('kasir'),
+                'role' => 'kasir',
+            ]
+        );
     }
 }
